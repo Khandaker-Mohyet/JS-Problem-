570d029858e79f268309521d6d49e2114dac8f52
Khandaker-Mohyet <mohyet008@gmail.com>
2025-02-24 01:16:39 +0600

    frist commit

---
+++ Variable/Task-3/practice-3.js
@@ -0,0 +1,8 @@
+const name = "Khandaker Mohyet";
+const address = "Toargoan, Kapasia, Gazipur";
+const deeder = "Rajshi University";
+const fan = "Sabnur";
+const movieOfTheYear = "I haven't seen any movies this year.";
+const hobby = "Vegetable gardening";
+const color = "Green"
+console.log(name, address, deeder, fan, movieOfTheYear, hobby, color)
---
+++ index.js
@@ -0,0 +1,1 @@
+console.log("Khandaker Mohyet js Journy")
