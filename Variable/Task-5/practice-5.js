87f6ccc73f27ebdfeb3f45cdd6441afcfdecd06b
Khandaker Mohyet <mohyet0007@gmail.com>
2025-02-24 18:27:35 +0600

    jabascript math 3

---
+++ Variable/Task-5/practice-5.js
@@ -0,0 +1,15 @@
+const income = 100;
+const cost = 40;
+const newBalance = income - cost;
+// console.log(newBalance)
+
+const totalPencil = 10;
+const allTimeUsed = 2;
+const TotalUsed = totalPencil / allTimeUsed;
+// console.log(TotalUsed)
+
+const money = 60;
+const usedAllTime = 15;
+const usedTotal = money / usedAllTime;
+const genitalResidue = money % usedAllTime;
+console.log(genitalResidue, usedTotal)
