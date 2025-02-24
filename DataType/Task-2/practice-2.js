128f26193bafc2d5566fe4b0b779438d73ddf40b
Khandaker-Mohyet <mohyet008@gmail.com>
2025-02-25 02:39:40 +0600

    data type task-2

---
+++ DataType/Task-2/practice-2.js
@@ -0,0 +1,22 @@
+let age = 26;
+age = 27;
+
+let pocketMoney = 50;
+pocketMoney = 150;
+
+let studyTime = 4;
+studyTime = 3;
+
+let friendsCount = 3;
+friendsCount = 1;
+
+let phonePrice = 25000;
+phonePrice = 12000;
+
+let chairsCount = 40;
+chairsCount = chairsCount - 2;
+chairsCount = chairsCount - 6;
+
+let playTime = 3;
+playTime = 0;
+console.log(age, pocketMoney, studyTime, friendsCount, phonePrice, chairsCount, playTime)
