f7d11a2447cdd64a9510980eb6185267babf81b0
Khandaker Mohyet <mohyet0007@gmail.com>
2025-02-27 02:07:50 +0600

    data type task 3

---
+++ DataType/Task-3/practice-3.js
@@ -0,0 +1,28 @@
+//Masters Prily Result
+const score = 3.11;
+
+// I don'n know what this radius
+const radius = 2.3;
+
+// My favorite dress
+const dress = 3;
+
+// this is gift money
+const giftMoney = 100;
+
+
+/*
+First hour I study in Math
+Second hour I study in coding
+*/
+const studyTime = 4;
+
+/*
+ product price 
+ delevary fee
+*/
+const sendMoney = 120;
+
+
+// price 300 dalevary pay 130
+const pay = 430
