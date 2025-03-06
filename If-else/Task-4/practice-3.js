06e9dbd2ed2835a93172f9fa7a6b2e4fda06716a
Khandaker Mohyet <mohyet0007@gmail.com>
2025-03-06 16:12:59 +0600

    if else doen

---
+++ If-else/Task-4/practice-3.js
@@ -0,0 +1,12 @@
+const isActive = true;
+const subscription = true;
+const isPrime = true;
+if (isActive) {
+  if (subscription) {
+    if (isPrime) {
+      console.log('look prime feature')
+    } else {
+      console.log('look free version')
+    }
+  }
+}
