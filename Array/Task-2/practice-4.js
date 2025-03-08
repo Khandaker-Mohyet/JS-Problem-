af586c6ab92d39ceba4039bd9626bdde0d05f425
Khandaker Mohyet <mohyet0007@gmail.com>
2025-03-08 16:56:17 +0600

    array task two doen

---
+++ Array/Task-2/practice-4.js
@@ -0,0 +1,3 @@
+const gameName = ["free fair", "pupje", "candicrash", "tample run"]
+gameName[2]= "subway sarfar"
+console.log(gameName)
---
+++ Array/Task-2/practice-6.js
@@ -0,0 +1,2 @@
+const country = ["India", "Pakistan", "Swizerland", "Egept", "England", "Canada", "Saiduriba"]
+console.log(country[4])
