d06e713135cbc83f00cc8b305b0db58ab0c31e98
Khandaker-Mohyet <mohyet008@gmail.com>
2025-03-08 22:06:38 +0600

    array task three doen

---
+++ Array/Task-3/practice-2.js
@@ -0,0 +1,3 @@
+const friendsName = ['sajib', 'sagor', 'sakil', 'sohal']
+friendsName.push('sumon')
+console.log(friendsName)
---
+++ Array/Task-3/practice-4.js
@@ -0,0 +1,3 @@
+const num = [24, 36, 48, 60]
+num.unshift(12);
+console.log(num)
