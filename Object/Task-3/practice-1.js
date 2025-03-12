7475da1a80fc2652d6eb7219c5b9c1b605cd403c
Khandaker Mohyet <mohyet0007@gmail.com>
2025-03-12 19:18:32 +0600

    object task three doen

---
+++ Object/Task-3/practice-1.js
@@ -0,0 +1,7 @@
+const book = {
+  name: "marhaba Jabascript a maro Thaba",
+  writer: "Jhankaer Mahabub",
+  price: 585
+}
+console.log(Object.keys(book))
+console.log(Object.values(book))
---
+++ Object/Task-3/practice-5.js
@@ -0,0 +1,6 @@
+const bike = {
+  brand: "Hero",
+  price: 120000,
+  model: "Splendor"
+}
+console.log(Object.values(bike))
