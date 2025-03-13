2227015b5c904c14d0a09a6ab1c98e9675275ee5
Khandaker Mohyet <mohyet0007@gmail.com>
2025-03-14 02:30:38 +0600

    function task one doen

---
+++ Function/Task-1/practice-4.js
@@ -0,0 +1,8 @@
+function taskDone() {
+  console.log('join on the morning screm');
+  console.log('go to my workespace')
+  console.log('back to my home')
+  console.log('join on the afternoon screm')
+  console.log('Go to bed for sleep')
+}
+taskDone()
---
+++ Function/Task-1/practice-6.js
@@ -0,0 +1,8 @@
+function getFavFriends() {
+  console.log("Deeder")
+  console.log("Ratul")
+  console.log("Ibrahim")
+  console.log("Jahid")
+  console.log("Ariful")
+}
+getFavFriends()
