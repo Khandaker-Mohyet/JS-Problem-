b0b33dca80d77ef68b6a0c2ed8ed17a792a4c270
Khandaker Mohyet <mohyet0007@gmail.com>
2025-04-10 02:05:22 +0600

    string mathod task four doen

---
+++ StringMathod/Task-4/practice-2.js
@@ -0,0 +1,3 @@
+const str = 'I am learning JavaScript. JavaScript is amazing';
+const result = str.replace("JavaScript", "JS")
+console.log(result)
---
+++ StringMathod/Task-4/practice-3.js
@@ -0,0 +1,3 @@
+const str = 'I love apples and apples are testy!';
+const result = str.replaceAll("apples", "mango")
+console.log(result)
