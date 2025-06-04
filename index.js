3a7e6c2be5c769c7ef4d1649e478e02a541e9142
Khandaker-Mohyet <mohyet008@gmail.com>
2025-06-04 21:22:31 +0600

    uncommend the index file

--- index.js
+++ index.js
@@ -1,73 +1,73 @@
-// console.log("Khandaker Mohyet js Journy")
+console.log("Khandaker Mohyet js Journy")
 
-// One
-// console.log("start");
-// setTimeout(() => {
-//   console.log("Timeout");
-// }, 0)
-// Promise.resolve().then(() => console.log("promise"));
-// console.log("End")
+One
+console.log("start");
+setTimeout(() => {
+  console.log("Timeout");
+}, 0)
+Promise.resolve().then(() => console.log("promise"));
+console.log("End")
 
-// three
-// function User(name) {
-//   this.name = name;
-// }
+three
+function User(name) {
+  this.name = name;
+}
 
-// User.prototype.sayHello = () => {
-//   console.log(`Hello, ${this.name}`);
-// }
+User.prototype.sayHello = () => {
+  console.log(`Hello, ${this.name}`);
+}
 
-// const user1 = new User("Alice");
-// user1.sayHello();
+const user1 = new User("Alice");
+user1.sayHello();
 
-// five
-// console.log(foo);
-// var foo = "Hello";
-// console.log(foo)
+five
+console.log(foo);
+var foo = "Hello";
+console.log(foo)
 
-// Thirtin
-// console.log(1+"2"+3)
+Thirtin
+console.log(1+"2"+3)
 
-// fifteen
-// function multiply(x, y=2) {
-//   return x * y;
-// }
-// console.log(multiply(5));
-// console.log(multiply(5, undefined));
-// console.log(multiply(5, null));
+fifteen
+function multiply(x, y=2) {
+  return x * y;
+}
+console.log(multiply(5));
+console.log(multiply(5, undefined));
+console.log(multiply(5, null));
 
-// sixteen
-// const a = [1, 2, 3];
-// const b = a
-// b.push(4);
-// console.log(a)
+sixteen
+const a = [1, 2, 3];
+const b = a
+b.push(4);
+console.log(a)
 
-// seventean
-// let x = 1;
-// let y = 2;
-// x = x + y++;
-// console.log(x, y);
+seventean
+let x = 1;
+let y = 2;
+x = x + y++;
+console.log(x, y);
 
-// ninteen
-// let x = [1, 2, 3];
-// let y = [...x];
-// y[0] = 4;
-// console.log(x[0], y[0])
+ninteen
+let x = [1, 2, 3];
+let y = [...x];
+y[0] = 4;
+console.log(x[0], y[0])
 
-// twenty
-// const numbers = [1, 2, 3];
-// numbers[10] = 10;
-// console.log(numbers.length)
+twenty
+const numbers = [1, 2, 3];
+numbers[10] = 10;
+console.log(numbers.length)
 
-// twenty one
-// let numbers = [1, 2, 3];
-// numbers = numbers.map(num => num * 2);
-// console.log(numbers)
+twenty one
+let numbers = [1, 2, 3];
+numbers = numbers.map(num => num * 2);
+console.log(numbers)
 
-// twenty two
-// const x = 10;
-// function foo() {
-//   console.log(x);
-//   const x = 20;
-// }
-// foo()
+twenty two
+const x = 10;
+function foo() {
+  console.log(x);
+  const x = 20;
+}
+foo()
